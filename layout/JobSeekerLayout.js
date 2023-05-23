b042ed7047a0585bd99c9de5fd5831424eeebbbc
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-05-23 18:34:05 +0100

    update

--- layout/JobSeekerLayout.js
+++ layout/JobSeekerLayout.js
@@ -1,7 +1,7 @@
+import Footer from "@/app/Components/Footer";
+import Header from "@/app/Components/Header";
+import Sidebar from "@/app/Components/Sidebar";
 import React from "react";
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 const JobSeekerLayout = ({ children }) => {
   return (
