6fdde29d8b669245cb3bd63abe21548ab201afe9
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-06-21 08:02:19 +0100

    update

--- pages/api/shareCountData.js
+++ pages/api/shareCountData.js
@@ -1,22 +1,9 @@
-// api/shareCountData.js
-
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  // Here, you would implement the logic to fetch the share count data
-  // for a specific resource or URL. This could involve making an API request
-  // to a social media platform or accessing a database.
-
-  // For demonstration purposes, let's assume you have a dummy share count data.
+export default function handler(req, res) {
   const shareCountData = {
-    facebook: 100,
-    twitter: 50,
-    linkedin: 75,
+    facebook: 123,
+    twitter: 456,
+    linkedin: 789,
   };
 
-  // Send the share count data as the response.
-  res.json(shareCountData);
-});
-
-module.exports = router;
+  res.status(200).json(shareCountData);
+}
